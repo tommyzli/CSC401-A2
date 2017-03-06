22659672de956e96a07fd893d4158f8577975a4c
Tommy Li <tommyli113@gmail.com>
2017-03-05 20:50:28 -0500

    WIP evalAlign

--- evalAlign.m
+++ evalAlign.m
@@ -5,30 +5,56 @@
 %  Task 5.
 
 % some of your definitions
-trainDir     = TODO;
-testDir      = TODO;
-fn_LME       = TODO;
-fn_LMF       = TODO;
-lm_type      = TODO;
-delta        = TODO;
-vocabSize    = TODO;
-numSentences = TODO;
+trainDir     = '/u/cs401/A2_SMT/data/Hansard/Training/';
+testDir      = '/u/cs401/A2_SMT/data/Hansard/Testing/';
+fn_LME       = 'fn_LME.mat';
+fn_LMF       = 'fn_LMF.mat';
+lm_type      = '';
+delta        = 0;
+vocabSize    = 0;
+numSentences = {1000, 10000, 15000, 30000};
 
 % Train your language models. This is task 2 which makes use of task 1
-LME = lm_train( trainDir, 'e', fn_LME );
-LMF = lm_train( trainDir, 'f', fn_LMF );
+if exist(fn_LME, 'file') == 2
+  load(fn_LME, '-mat');
+else
+  LME = lm_train( trainDir, 'e', fn_LME );
+end
 
-% Train your alignment model of French, given English
-AMFE = align_ibm1( trainDir, numSentences );
-% ... TODO: more
+if exist(fn_LMF, 'file') == 2
+  load(fn_LMF, '-mat');
+else
+  LMF = lm_train( trainDir, 'f', fn_LMF );
+end
 
-% TODO: a bit more work to grab the English and French sentences.
-%       You can probably reuse your previous code for this
+testing_french_lines = textread(strcat(testDir, 'Task5.f'), '%s', 'delimiter', '\n');
+testing_english_lines = textread(strcat(testDir, 'Task5.e'), '%s', 'delimiter', '\n');
+testing_google_english_lines = textread(strcat(testDir, 'Task5.google.e'), '%s', 'delimiter', '\n');
 
-% Decode the test sentence 'fre'
-eng = decode( fre, LME, AMFE, 'smooth', delta, vocabSize );
+% Train your alignment model of French, given English for each numSentences
+for i=1:length(numSentences)
+  disp('=========================================================')
+  disp(strcat('TRAINING ON  ', num2str(numSentences{i}), ' SENTENCES'))
+  disp('=========================================================')
+  aligned_model_file_name = strcat('fn_AM_', num2str(numSentences{i}), '.mat');
+  AMFE = align_ibm1( trainDir, numSentences{i}, 10, aligned_model_file_name );
 
-% TODO: perform some analysis
-% add BlueMix code here
+  for sentence_index=1:length(testing_french_lines)
+    fre = preprocess(testing_french_lines{sentence_index}, 'f');
+    % Decode the test sentence 'fre'
+    eng = decode2( fre, LME, AMFE, lm_type, delta, vocabSize );
+    disp('original line:')
+    testing_french_lines{sentence_index}
+    disp('my translation:')
+    eng
+    disp('googles translation:')
+    testing_google_english_lines{sentence_index}
+    disp('actual translation:')
+    testing_english_lines{sentence_index}
+
+    % TODO: perform some analysis
+    % add BlueMix code here
+  end
+end
 
 [status, result] = unix('')
